5610d70228873c3fd476115d5aaee55deaedbc77
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-10 17:43:32 +0300

    feat: add SplashActivity with logo

---
+++ app/src/main/java/com/example/beeradviser/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.beeradviser;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 2000; // 2 секунды
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Закрыть SplashActivity
+        }, SPLASH_DELAY);
+    }
+}
