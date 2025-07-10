35c10b60c46bf0cb3a9a28d6d324d9b7d96bbc39
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-10 17:57:32 +0300

    feat: extend BeerExpert with recommendations for light, brown, and dark beers

--- app/src/main/java/com/example/beeradviser/MainActivity.java
+++ app/src/main/java/com/example/beeradviser/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.beeradviser;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button findBeerButton = findViewById(R.id.find_beer);
+        TextView brands = findViewById(R.id.brands);
+
+        BeerExpert expert = new BeerExpert();
+
+        findBeerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner color = findViewById(R.id.color);
+                String beerType = String.valueOf(color.getSelectedItem());
+
+                List<String> brandsList = expert.getBrands(beerType);
+                StringBuilder brandsFormatted = new StringBuilder();
+                for (String brand : brandsList) {
+                    brandsFormatted.append(brand).append('\n');
+                }
+
+                // output
+                brands.setText(brandsFormatted);
+            }
+        });
+
     }
 }
