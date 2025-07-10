538ff94b5a2bf0897f37d908b6e29e71bb460091
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-10 17:11:53 +0300

    feat: extend BeerExpert with recommendations for light, brown, and dark beers

--- app/src/main/java/com/example/beeradviser/BeerExpert.java
+++ app/src/main/java/com/example/beeradviser/BeerExpert.java
@@ -6,12 +6,27 @@ import java.util.List;
 public class BeerExpert {
     List<String> getBrands (String color) {
         List<String> brands = new ArrayList<>();
-        if (color.equals("amber")) {
-            brands.add("Jack Amber");
-            brands.add("Red Moose");
-        } else {
-            brands.add("Jail Pale Ale");
-            brands.add("Gout Stout");
+
+        switch (color) {
+            case "amber":
+                brands.add("Jack Amber");
+                brands.add("Red Moose");
+                break;
+            case "light":
+                brands.add("Blue Moon");
+                brands.add("Corona Extra");
+                break;
+            case "brown":
+                brands.add("Brown Bear Beer");
+                brands.add("Bock Lager");
+                break;
+            case "dark":
+                brands.add("Gout Stout");
+                brands.add("Dark Daniel");
+                break;
+            default:
+                brands.add("Generic Beer");
+                break;
         }
         return brands;
     }
